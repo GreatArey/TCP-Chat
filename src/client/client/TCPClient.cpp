cb5ac03d2d0319064e7b9ac0b3d825433f56d960
Shinde <petrenko200903@gmail.com>
2024-05-11 20:36:52 +0700

    Add port verification in client programm

--- src/client/client/TCPClient.cpp
+++ src/client/client/TCPClient.cpp
@@ -22,6 +22,14 @@ int main(int argc, char *argv[])
 {
     unsigned int port = MCL::get_port(argc, argv);
 
+    if (port == 0)
+    {
+        std::cout << "Usage:\n\n"
+                  << "./server port\n\n"
+                  << "port: unsigned int value, greater than 0\n";
+        return 0;
+    }
+
     MCL::TCPClient client{port};
 
     client.ConnectToServer();
